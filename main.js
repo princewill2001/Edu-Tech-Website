625aab716bb26848ec914bc4d238003ee4e63624
princewill2001 <pryncwill819@gmail.com>
2024-01-19 15:13:44 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// CHANGE NAVBAR STYLES ON SCROLL //
+
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0); // Immediately invokes function expression to scroll the navbar
+});
+
+// SHOW/HIDE FAQ ANSWER //
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    // CHANGE ICON //
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "uil uil-plus-circle") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus-circle";
+    }
+  });
+});
+
+// SHOW/HIDE TABLET NAVBAR //
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+closeBtn.addEventListener("click", closeNav);
